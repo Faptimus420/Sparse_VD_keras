609a274a89d3bec970536d909980c2ede3bb6b39
Faptimus420 <patrik.zori@protonmail.com>
2023-09-28 16:58:58 +0200

    Initial commit * Translated VariationalDense to Keras Core API ** Note: Did not find a backend-agnostic implementation of tf.random.normal in the Keras Core API, had to use the one in keras_core.backend.tensorflow for now - not sure if this is backend-agnostic * Implemented the kernel_regularizer argument in VariationalDense, passing it to the theta kernel builder * Implemented the get_config method, allowing the layer to be serialized

--- README.md
+++ README.md
@@ -1,22 +1,13 @@
-# Sparse_VD_tf2
-Implementation of Sparse Variational Dropout in Tensorflow2!
+# Sparse_VD_keras
+Reimplementation of Sparse Variational Dropout in Keras-Core/Keras 3.0, to make it backend-agnostic.
+In addition, provides functionality to export/serialize a model which makes use of the custom layers, and implements the ability to use Keras kernel regularizers in the custom layers.
+(The above functionality was implemented to allow using orthogonality regularization, to comply with the process described in Improving Word Embedding Using Variational Dropout (FLAIRS 2023) https://journals.flvc.org/FLAIRS/article/view/133326)
 
-Original paper: Variational Dropout Sparsifies Deep Neural Networks(ICML 2017) https://arxiv.org/pdf/1701.05369.pdf
-
+Original paper: Variational Dropout Sparsifies Deep Neural Networks (ICML 2017) https://arxiv.org/pdf/1701.05369.pdf
 Official repo: https://github.com/bayesgroup/variational-dropout-sparsifies-dnn
+
+Forked from Cerphilly's TensorFlow 2 implementation: https://github.com/Cerphilly/Sparse_VD_tf2
 
 ## Requirements
 
-* TensorFlow >= 2.0
-
-```shell
-pip install tensorflow
-```
-
-## How to Test
-
-run LeNet.py to test training of LeNet5 by MNIST dataset.
-
-## Result
-
-96% accuracy & 95% sparsity after 20 epochs of training. 
+* keras-core >= 0.1.7 (OR keras >= 3.0, once released)
--- VariationalDense.py
+++ VariationalDense.py
@@ -1,41 +1,43 @@
-import tensorflow as tf
-import numpy as np
-from tensorflow.python.keras import activations
+from keras_core import activations, initializers, regularizers, ops
+from keras_core.layers import Layer
+from keras_core.backend.tensorflow.random import normal
 
-class VariationalDense(tf.keras.layers.Layer):
-    def __init__(self, output_dim, use_bias=True, threshold=3.0, activation=None, kernel_initializer='glorot_normal', bias_initializer='zeros'):
+class VariationalDense(Layer):
+    def __init__(self, output_dim, use_bias=True, threshold=3.0, activation=None, kernel_initializer='glorot_normal', bias_initializer='zeros', kernel_regularizer=None):
         super(VariationalDense, self).__init__()
         self.output_dim = output_dim
         self.use_bias = use_bias
         self.threshold = threshold
         self.activation = activations.get(activation)
 
-        self.kernel_initializer = kernel_initializer
-        self.bias_initializer = bias_initializer
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
 
     def build(self, input_shape):
         self.theta = self.add_weight("kernel", shape=(int(input_shape[-1]), self.output_dim),
-                                     initializer=self.kernel_initializer, trainable=True)
+                                     initializer=self.kernel_initializer, trainable=True, regularizer=self.kernel_regularizer)
 
         self.log_sigma2 = self.add_weight("log_sigma2", shape=(int(input_shape[-1]), self.output_dim),
-                                 initializer=tf.constant_initializer(-10.0), trainable=True)
+                                 initializer=initializers.Constant(-10.0), trainable=True)
 
         if self.use_bias == True:
             self.bias = self.add_weight("bias", shape=(self.output_dim,),
                                         initializer=self.bias_initializer, trainable=True)
 
     def sparsity(self):
-        total_param = np.prod(tf.shape(self.boolean_mask))
-        remaining_param = tf.math.count_nonzero(tf.cast(self.boolean_mask, dtype=tf.uint8)).numpy()
+        total_param = ops.prod(ops.shape(self.boolean_mask))
+        remaining_param = ops.count_nonzero(ops.cast(self.boolean_mask, dtype="uint8"))
 
         return remaining_param, total_param
 
     @property
     def log_alpha(self):
-        theta = tf.where(tf.math.is_nan(self.theta), tf.zeros_like(self.theta), self.theta)
-        log_sigma2 = tf.where(tf.math.is_nan(self.log_sigma2), tf.zeros_like(self.log_sigma2), self.log_sigma2)
-        log_alpha = tf.clip_by_value(log_sigma2 - tf.math.log(tf.square(theta) + 1e-10), -20.0, 4.0)
-        return tf.where(tf.math.is_nan(log_alpha), self.threshold * tf.ones_like(log_alpha), log_alpha)
+        theta = ops.where(ops.isnan(self.theta), ops.zeros_like(self.theta), self.theta)
+        log_sigma2 = ops.where(ops.isnan(self.log_sigma2), ops.zeros_like(self.log_sigma2), self.log_sigma2)
+        log_alpha = ops.clip(log_sigma2 - ops.log(ops.square(theta) + 1e-10), -20.0, 4.0)
+        return ops.where(ops.isnan(log_alpha), self.threshold * ops.ones_like(log_alpha), log_alpha)
 
 
     @property
@@ -44,24 +46,23 @@ class VariationalDense(tf.keras.layers.Layer):
 
     @property
     def sparse_theta(self):
-        theta = tf.where(tf.math.is_nan(self.theta), tf.zeros_like(self.theta), self.theta)
-        return tf.where(self.boolean_mask, theta, tf.zeros_like(theta))
+        theta = ops.where(ops.isnan(self.theta), ops.zeros_like(self.theta), self.theta)
+        return ops.where(self.boolean_mask, theta, ops.zeros_like(theta))
 
     @property
     def regularization(self):
         k1, k2, k3 = 0.63576, 1.8732, 1.48695
         C = -k1
-        mdkl = k1 * tf.sigmoid(k2 + k3 * self.log_alpha) - 0.5 * tf.math.log(1 + (tf.exp(-self.log_alpha))) + C
+        mdkl = k1 * ops.sigmoid(k2 + k3 * self.log_alpha) - 0.5 * ops.log(1 + (ops.exp(-self.log_alpha))) + C
 
-        return -tf.reduce_sum(mdkl)
+        return -tf.sum(mdkl)
 
-    @tf.function
     def call(self, input, sparse=False):
         if not sparse:
-            theta = tf.where(tf.math.is_nan(self.theta), tf.zeros_like(self.theta), self.theta)
-            sigma = tf.sqrt(tf.exp(self.log_alpha) * theta * theta)
-            self.weight = theta + tf.random.normal(tf.shape(theta), 0.0, 1.0) * sigma
-            output = tf.matmul(input, self.weight)
+            theta = ops.where(ops.isnan(self.theta), ops.zeros_like(self.theta), self.theta)
+            sigma = ops.sqrt(ops.exp(self.log_alpha) * theta * theta)
+            self.weight = theta + normal(ops.shape(theta), 0.0, 1.0) * sigma    # Not sure if this line is backend-agnostic or TF-only - not yet implemented in the Keras Core ops API
+            output = ops.matmul(input, self.weight)
             if self.use_bias == True:
                 output += self.bias
             if self.activation is not None:
@@ -70,7 +71,7 @@ class VariationalDense(tf.keras.layers.Layer):
             return output
 
         else:
-            output = tf.matmul(input, self.sparse_theta)
+            output = ops.matmul(input, self.sparse_theta)
             if self.use_bias == True:
                 output += self.bias
             if self.activation is not None:
@@ -78,11 +79,28 @@ class VariationalDense(tf.keras.layers.Layer):
 
             return output
 
+    def get_config(self):
+        base_config = super().get_config()
+        config = {
+            "output_dim": self.output_dim,
+            "activation": activations.serialize(self.activation),
+            "use_bias": self.use_bias,
+            "threshold": self.threshold,
+            "kernel_initializer": initializers.serialize(
+                self.kernel_initializer
+            ),
+            "bias_initializer": initializers.serialize(self.bias_initializer),
+            "kernel_regularizer": regularizers.serialize(
+                self.kernel_regularizer
+            ),
+        }
+        return {**base_config, **config}
+
 
 
 if __name__ == '__main__':
     a = VariationalDense(10)
-    print(a(tf.zeros((1, 1)), sparse=True))
+    print(a(ops.zeros((1, 1)), sparse=True))
     print(a.regularization)
 
 
