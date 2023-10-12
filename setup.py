40204becad2c75899351505842cdcc4380b628a3
Faptimus420 <patrik.zori@protonmail.com>
2023-10-12 18:44:17 +0200

    Setup.py bug

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Sparse Variational Dropout layers for Keras Core/3.0',
     url='https://github.com/Faptimus420/Sparse_VD_keras-core',
     author='Patrik Zori',
-    packages=['sparse_vd_keras'],
+    packages=['sparse_vd_keras/'],
     install_requires=['keras_core>=0.1.7'],
     py_modules=['VariationalConv2d', 'VariationalDense']
 )
