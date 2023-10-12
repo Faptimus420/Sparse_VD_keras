8e57fa8890b403041f11166adc072b5edf16bbe9
Patrik Žori <patrik.zori@protonmail.com>
2023-10-12 22:30:14 +0200

    Update setup.py
    Setup.py bugfix

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='sparse-vd-keras',
     version='1.0',
     description='Sparse Variational Dropout layers for Keras Core/3.0',
     url='https://github.com/Faptimus420/Sparse_VD_keras-core',
-    author='Patrik Zori',
-    packages=find_packages(exclude=['LeNet.py']),
+    author='Cerphilly and Patrik Zori',
+    py_modules=['variationalconv2d', 'variationaldense'],
     install_requires=['keras_core>=0.1.7'],
     python_requires='>=3.9',
 )
